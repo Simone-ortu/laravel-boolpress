11900d50a2e9134826516de1635e9038071414fa
Shinnolo <simo.28.09o@gmai.com>
2021-03-25 16:09:27 +0100

    Guest route

--- laravel-boolpress/app/Http/Controllers/Admin/HomeController.php
+++ laravel-boolpress/app/Http/Controllers/Admin/HomeController.php
@@ -1,17 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         return view('guest.home');
