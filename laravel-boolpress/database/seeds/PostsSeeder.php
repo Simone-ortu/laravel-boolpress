fe016d6943b6f28e6b8f563fd43a1397afd547ca
Shinnolo <simo.28.09o@gmai.com>
2021-04-01 15:04:34 +0200

    for cicle in PostsSeeder
    pt.2

--- laravel-boolpress/database/seeds/PostsSeeder.php
+++ laravel-boolpress/database/seeds/PostsSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Post;
 use Illuminate\Database\Seeder;
 
 class PostsSeeder extends Seeder
@@ -9,8 +10,25 @@ class PostsSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 10; $i++){
+            $newPost = new Post();
+
+            $newPost->title =  $faker->sentence(3);
+            $ifSlugExists = Post::where('slug',$slug)->first();
+            $tempSlug = $slug;
+            $counter =  1;
+
+            while($ifSlugExists){
+                $slug = $tempSlug . '-' . $counter;
+                $ifSlugExists = Post::where('slug',$slug)->first();
+                $counter++;
+            }
+            $newPost->slug = $slug;
+            $newPost->content = $faker-text();
+
+            $newPost->save();
+        }
     }
 }
