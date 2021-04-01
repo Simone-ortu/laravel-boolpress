1c32657cf16943fb62b44880936dc22082274ea3
Shinnolo <simo.28.09o@gmai.com>
2021-04-01 12:31:11 +0200

    Route

--- laravel-boolpress/routes/web.php
+++ laravel-boolpress/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('guests.home');
+// });
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')
+->namespace('Admin')
+->middleware('auth')
+->group(function () {
+Route::get('/', 'HomeController@index')
+
+->name('home');
+});
